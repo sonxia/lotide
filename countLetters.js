11330fbc1fe193450c390e4c801feef47e1ec417
Sonia Mudhar <sonia.mudhar@ucalgary.ca>
2023-02-03 00:22:29 +0000

    implement countLetters in Lotide

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🥰 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😔 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const result = {};
+  const newString = string.split(" ").join("");
+  for (const letter of newString) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+console.log(countLetters('LHL'))
+console.log(countLetters('lighthouse in the house'))
