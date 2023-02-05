afa13e1c50c041ce9aa157cd56643f87a66d21f3
Sonia Mudhar <sonia.mudhar@ucalgary.ca>
2023-02-05 10:31:29 +0000

    first five lotid objects

---
+++ eqObjects.js
@@ -0,0 +1,47 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🥰 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😔 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(a,b){
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+      return false; 
+    }
+    for (let a of Object.keys(object1)) {
+      if (Array.isArray(object1[a]) && Array.isArray(object2[a])) { 
+        return eqArrays(object1[a], object2[a]) 
+      }
+      if (object1[a] !== object2[a]) {
+        return false;
+      }
+  }
+  return true;
+};
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+console.log(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject)); 
+
+const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+console.log(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject)); 
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+console.log(eqObjects(shirtObject , anotherShirtObject)); 
+console.log(assertEqual(eqObjects(shirtObject , anotherShirtObject), true));
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+console.log(eqObjects(shirtObject , longSleeveShirtObject)); 
+console.log(assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false));
---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🥰 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😔 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue  = (object,value) => {
+  for (const objectKey in object) {
+    if (object[objectKey] === value) {
+      return objectKey;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  psychological: "Hannibal",
+  action: "Moon Knight",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Hannibal"), "psychological")
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Moon Knight"), "action")
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Lucifer", undefined))
