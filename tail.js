99146070c6b178e9e454bb3c749b7ffb231bfc43
Sonia Mudhar <sonia.mudhar@ucalgary.ca>
2023-02-22 23:11:24 +0000

    refractored tail.js

--- tail.js
+++ tail.js
@@ -1,16 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🥰 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`😔 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 function tail(assertEqual) {
   return assertEqual.slice(1);
 }
 
-//compare the values of the returned tail arrays otherwise the tail function will always fail
-const result = tail(["Yeo", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); //length of the result is 2
-assertEqual(result[0], "Lighthouse"); //the item in position 0 is Lighthouse
+module.exports = tail;
+
