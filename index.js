79544955060b3c9a1635a41213693af599e42286
Sonia Mudhar <sonia.mudhar@ucalgary.ca>
2023-02-23 04:57:53 +0000

    refractored using property value shorthand

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require("./assertEqual")
-const assertObjectsEqual   = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
 const eqArrays = require('./eqArrays');
@@ -14,22 +11,21 @@ const flatten = require('./flatten');
 const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
+const letterPositions = require('./letterPositions');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  map,
+  takeUntil,
+  without,
+  letterPositions
 };
